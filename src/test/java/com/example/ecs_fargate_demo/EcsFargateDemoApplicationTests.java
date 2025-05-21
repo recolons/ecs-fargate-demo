6699bcd61fc29f9ece27ac55e101d5ee9c4dab53
ruben <rubene.colons@gmail.com>
2025-05-21 13:47:05 -0400

    configure h2 db for tests

--- src/test/java/com/example/ecs_fargate_demo/EcsFargateDemoApplicationTests.java
+++ src/test/java/com/example/ecs_fargate_demo/EcsFargateDemoApplicationTests.java
@@ -2,8 +2,18 @@ package com.example.ecs_fargate_demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+})
 class EcsFargateDemoApplicationTests {
 
 	@Test
