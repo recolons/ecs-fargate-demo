8ec036faa2c496daa1e90493be0f607cfd6cfb54
ruben <rubene.colons@gmail.com>
2025-05-21 13:50:37 -0400

    tests config try 2

--- src/test/java/com/example/ecs_fargate_demo/EcsFargateDemoApplicationTests.java
+++ src/test/java/com/example/ecs_fargate_demo/EcsFargateDemoApplicationTests.java
@@ -4,15 +4,22 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import com.example.ecs_fargate_demo.repository.ArticleRepository;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@DataJpaTest
+@Import(ArticleRepository.class)
 @TestPropertySource(properties = {
     "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
     "spring.datasource.username=sa",
     "spring.datasource.password=",
     "spring.datasource.driver-class-name=org.h2.Driver",
-    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.jpa.show-sql=true"
 })
 class EcsFargateDemoApplicationTests {
 
