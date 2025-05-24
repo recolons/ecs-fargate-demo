593af2f778c7e379e66ad03fdb21c8db5bb5ffa3
ruben <rubene.colons@gmail.com>
2025-05-24 17:07:48 -0400

    configure to remove local profile in various app and config files

--- src/main/java/com/example/ecs_fargate_demo/EcsFargateDemoApplication.java
+++ src/main/java/com/example/ecs_fargate_demo/EcsFargateDemoApplication.java
@@ -2,12 +2,18 @@ package com.example.ecs_fargate_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class EcsFargateDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcsFargateDemoApplication.class, args);
+		// Force prod profile and disable local profile
+		System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "prod");
+		System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "prod");
+		SpringApplication app = new SpringApplication(EcsFargateDemoApplication.class);
+		app.setAdditionalProfiles("prod");
+		app.run(args);
 	}
 
 }
