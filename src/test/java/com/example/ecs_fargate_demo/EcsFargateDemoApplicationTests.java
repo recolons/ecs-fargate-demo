f5af18ed25a8bc288775cb205b96b340f509b785
ruben <rubene.colons@gmail.com>
2025-05-26 09:30:31 -0400

    changes to tests file

--- src/test/java/com/example/ecs_fargate_demo/EcsFargateDemoApplicationTests.java
+++ src/test/java/com/example/ecs_fargate_demo/EcsFargateDemoApplicationTests.java
@@ -3,14 +3,9 @@ package com.example.ecs_fargate_demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
-import com.example.ecs_fargate_demo.repository.ArticleRepository;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@DataJpaTest
-@Import(ArticleRepository.class)
 class EcsFargateDemoApplicationTests {
 
 	@Test
